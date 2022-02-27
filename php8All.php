3df9b04db6d3907fd2c0219c14d415170530d7d4
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-27 14:28:59 +0700

    All

--- php8All.php
+++ php8All.php
@@ -4,5 +4,40 @@
 
 echo "Belajar Fitur PHP 8" . PHP_EOL;
 
+Named Argument
+Biasanya saat kita memanggil function, maka kita harus memasukkan argument atau parameter sesuai dengan posisinya
+Dengan kemampuan named argument, kita bisa memasukkan argument atau parameter tanpa harus mengikuti posisi nya
+Namun penggunaan named argument harus disebutkan nama argument atau parameter nya
+Named argument juga menjadikan kode program mudah dibaca ketika memanggil function yang memiliki argument yang sangat banyak
+https://wiki.php.net/rfc/named_params 
+
+Kode : Function
+function sayHello(string $first, string $middle = "", string $last): void
+{
+    echo "Hello $first $middle $last" . PHP_EOL;
+}
+
+Kode : Named Argument
+sayHello("Eko", "Kurniawan", "Khannedy");
+// sayHello("Eko", "Khannedy"); // error
+
+sayHello(last: "Khannedy", first: "Eko", middle: "Kurniawan");
+sayHello(first: "Eko", last: "Khannedy");
+
+Kode : Function Default Argument
+function sayHello(string $first, string $middle = "", string $last): void
+{
+    echo "Hello $first $middle $last" . PHP_EOL;
+}
+
+Kode : Named Argument Default Value
+sayHello("Eko", "Kurniawan", "Khannedy");
+// sayHello("Eko", "Khannedy"); // error
+
+sayHello(last: "Khannedy", first: "Eko", middle: "Kurniawan");
+sayHello(first: "Eko", last: "Khannedy");
+
+
+
 
 */
