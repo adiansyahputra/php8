ad1a04da5f857e85ced6ea1ba200a3749c3385b2
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-28 09:57:09 +0700

    ALL

--- php8All.php
+++ php8All.php
@@ -294,4 +294,30 @@ $result = match (true) {
 
 echo $result . PHP_EOL;
 
+Nullsafe Operator
+PHP sekarang memiliki nullsafe operator seperti di bahasa pemrograman Kotlin atau TypeScript
+Biasanya ketika kita ingin mengakses sesuatu dari sebuah object yang bisa memungkinan nilai null, maka kita akan melakukan pengecekan apakah object tersebut null atau tidak, jika tidak baru kita akses object tersebut
+Dengan nullsafe operator, kita tidak perlu melakukan itu, kita hanya perlu menggunakan karakter ? (tanda tanya), secara otomatis PHP akan melakukan pengecekan null tersebut 
+https://wiki.php.net/rfc/nullsafe_operator 
+
+Kode : Nullable Class
+class Address
+{
+    public ?string $country;
+}
+
+class User
+{
+    public ?Address $address;
+}
+
+Kode : Nullsafe Operator
+function getCountry(?User $user): ?string
+{
+    return $user?->address?->country;
+}
+
+echo getCountry(null) . PHP_EOL;
+
+
 */
