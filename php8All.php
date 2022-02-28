267ca1e1c645878962ea63421019bd2715765b93
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-28 10:04:13 +0700

    ALL

--- php8All.php
+++ php8All.php
@@ -319,5 +319,15 @@ function getCountry(?User $user): ?string
 
 echo getCountry(null) . PHP_EOL;
 
+String to Number Comparison
+Salah satu yang membingungkan di PHP adalah ketika kita melakukan perbandingan number dan string
+Misal saat kita bandingkan 0 == “eko”, maka hasilnya true
+Kenapa true? Karena PHP akan melakukan type jugling dan mengubah “eko” menjadi 0, sehingga hasilnya true
+Di PHP 8, khusus perbandingan String ke Number diubah, agar tidak membingungkan
+https://wiki.php.net/rfc/string_to_number_comparison 
+
+String to Number Comparison di PHP 8
+
+
 
 */
