e42f582dc6b5a4bafefd5ad176cc2bd60efbb57a
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-28 09:47:51 +0700

    ALL

--- php8All.php
+++ php8All.php
@@ -229,6 +229,69 @@ function sampleFunction(string|array $data): string|array
 var_dump(sampleFunction("Eko"));
 var_dump(sampleFunction([]));
 
+Match Expression
+PHP 8 menambahkan struktur kontrol baru bernama match expression
+Match expression adalah struktur kontrol yang mirip dengan switch case, namun lebih baik
+Match adalah expression, artinya dia bisa mengembalikan value
+https://wiki.php.net/rfc/match_expression_v2
+https://www.php.net/manual/en/control-structures.match.php 
 
+Kode : Switch Statement
+$value = "E";
+$result = "";
+
+switch ($value) {
+    case "A":
+    case "B":
+    case "C":
+        $result = "Anda Lulus";
+        break;
+    case "D":
+        $result = "Anda tidak lulus";
+        break;
+    case "E":
+        $result = "Mungkin Anda salah jurusan";
+        break;
+    default:
+        $result = "Nilai apa itu?";
+}
+
+Kode : Match Expression
+$result = match ($value) {
+    "A", "B", "C" => "Anda Lulus",
+    "D" => "Anda Tidak Lulus",
+    "E" => "Mungkin Anda salah jurusan",
+    default => "Nilai apa itu?"
+};
+
+echo $result . PHP_EOL;
+
+Non Equals Check di Match Expression
+Selain equals check, berbeda dengan switch case, di match expression, kita bisa melakukan pengecekan kondisi lainnya
+Misal pengecekan menggunakan kondisi perbandingan, bahkan pengecekan kondisi berdasarkan boolean expression yang dihasilkan dari sebuah function
+
+Kode : Match Expression Non Equals
+$value = 65;
+
+$result = match (true) {
+    $value >= 80 => "A",
+    $value >= 70 => "B",
+    $value >= 60 => "C",
+    $value >= 50 => "D",
+    default => "E"
+};
+
+echo $result . PHP_EOL;
+
+Kode : Match Expression dengan Kondisi
+$name = "Mrs. Nani";
+
+$result = match (true) {
+    str_contains($name, "Mr.") => "Hello Sir",
+    str_contains($name, "Mrs.") => "Hello Mam",
+    default => "Hello"
+};
+
+echo $result . PHP_EOL;
 
 */
