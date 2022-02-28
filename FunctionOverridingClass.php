6a60f2f254bdd906a21c0fae0cc782c52fffc1ca
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-28 11:14:39 +0700

    FunctionOverridingClass

---
+++ FunctionOverridingClass.php
@@ -0,0 +1,18 @@
+FunctionOverridingClass
+
+<?php
+
+class ParentClass
+{
+    public function method(string $name)
+    {
+    }
+}
+
+class ChildClass extends ParentClass
+{
+
+    public function method(int $name)
+    {
+    }
+}
