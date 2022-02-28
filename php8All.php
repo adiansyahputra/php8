2ce9ca91f68453cdc17321ae8bb712c2ae2cefb2
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-28 11:57:29 +0700

    ALL

--- php8All.php
+++ php8All.php
@@ -541,6 +541,30 @@ var_dump($login::class); // php 8
 var_dump(get_class($login));
 var_dump(Login::class);
 
+Stringable Interface
+Di PHP 8, sekarang diperkenalkan interface baru bernama Stringable
+Jika kita melakukan override magic function __toString, maka secara otomatis class kita akan implement interface Stringable
+Kita tidak perlu melakukannya secara manual, ini sudah dilakukan secara otomatis oleh PHP 8
+https://wiki.php.net/rfc/stringable 
+
+Kode : Function Argument Stringable
+function sayHello(Stringable $stringable): void
+{
+    echo "Hello {$stringable->__toString()}" . PHP_EOL;
+}
+
+Kode : Override toString Function
+class Person
+{
+
+    public function __toString(): string
+    {
+        return "Person";
+    }
+}
+
+sayHello(new Person());
+
 
 
 
