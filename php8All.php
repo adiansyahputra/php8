7ab9938912706c4ffabf04d6977ba542a6682160
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-28 11:51:34 +0700

    ALL

--- php8All.php
+++ php8All.php
@@ -525,6 +525,22 @@ function sayHelloExpression(?string $name)
 sayHelloExpression("Eko");
 sayHelloExpression(null);
 
+Allow ::class on Objects
+Di PHP 7, untuk mendapatkan nama class sebuah object, kita perlu menggunakan NamaClass::class atau get_class($object)
+Di PHP 8, sekarang kita bisa langsung mengambil nama class dari $object::class secara langsung
+https://wiki.php.net/rfc/class_name_literal_on_object 
+
+Kode : Allow ::class on Objects
+class Login
+{
+}
+
+$login = new Login();
+
+var_dump($login::class); // php 8
+var_dump(get_class($login));
+var_dump(Login::class);
+
 
 
 
