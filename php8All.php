dff088ecf6e5df3f241c05afb4f76a23d910a794
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-28 10:10:26 +0700

    ALL

--- php8All.php
+++ php8All.php
@@ -328,6 +328,14 @@ https://wiki.php.net/rfc/string_to_number_comparison
 
 String to Number Comparison di PHP 8
 
+Consistent Type Error
+Saat kita membuat function, dan ketika kita mengirim argument dengan tipe data yang salah, maka akan berakibat terjadi TypeError
+Sayangnya di PHP banyak function bawaan yang tidak mengembalikan TypeError, malah memberi warning
+Agar konsisten, sekarang di PHP 8, banyak function bawaan yang akan error TypeError jika kita salah mengirim tipe data
+https://wiki.php.net/rfc/consistent_type_errors 
+
+Kode : Consistent Type Error
+strlen([]);
 
 
 */
