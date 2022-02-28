89e4c20e255b3d3e1594c3281b20c1d8f67abe2b
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-28 11:47:25 +0700

    ALL

--- php8All.php
+++ php8All.php
@@ -509,6 +509,22 @@ try {
     echo "Invalid" . PHP_EOL;
 }
 
+Throw Expression
+Throw adalah sebuah statement
+Hal ini menyebabkan kadang kita kesulitan menggunakan throw di beberapa tempat yang membutuhkan expression, seperti ternary operator misalnya
+Di PHP 8, sekarang throw adalah sebuah expression, artinya dia memiliki nilai, dan sekarang kita bisa gunakan di tempat-tempat yang memang membutuhkan expression, seperti ternary operator
+https://wiki.php.net/rfc/throw_expression 
+
+Kode : Throw Expression
+function sayHelloExpression(?string $name)
+{
+    $result = $name != null ? "Hello $name" : throw new Exception("Tidak boleh null");
+    echo $result . PHP_EOL;
+}
+
+sayHelloExpression("Eko");
+sayHelloExpression(null);
+
 
 
 
